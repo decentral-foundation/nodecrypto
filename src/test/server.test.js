2acf556d1fbc959c8452405b1368f7480e5a3844
Jake Walsh <jkwalsh127@gmail.com>
2022-08-27 20:41:56 -0700

    combine tests for proper syntax

--- src/test/server.test.js
+++ src/test/server.test.js
@@ -14,16 +14,10 @@ test('test server routes, should receive an object with a key of updates', async
     const scope = await nock(`http://localhost:3500`)
         .get(`/v0/updates`)
         .reply(200, { updates: ["no new updates"]});
-
     const response = JSON.parse(scope.keyedInterceptors['GET http://localhost:3500/v0/updates'][0].body);
 
-    t.deepEqual(response, { updates :["no new updates"]}, 'json response should match expected');
-    t.end();
-})
-
-test('test server routes, should receive an object with a key of updates', async(t) => {
     const data = await getResponse();
 
-    t.deepEqual(data, { updates :["no new updates"]}, 'json response should match expected');
+    t.deepEqual(data, response, 'json response should match expected');
     t.end();
 })
