5b894f99f2249afdc8c0d24d0317fa90b34a3350
Jake Walsh <jkwalsh127@gmail.com>
2022-08-28 02:35:28 -0700

    provide more coverage for range function tests

--- src/test/range.test.js
+++ src/test/range.test.js
@@ -2,8 +2,28 @@ const test = require('tape');
 
 const { range } = require('../app/getCipherKey');
 
-test('range should return an array sorted from start to end', t => {
-    const result = range( 2, 8 )
-    t.deepEqual(result, [ 2, 3, 4, 5, 6, 7 ])
+
+for (let i = 0; i < 10; i++) {
+    test('range should return an array sorted from "start" to "end", given as parameters, when start is less than end', t => {
+        const result = range( i, i + 5 )
+
+        t.deepEqual(result, [ i, i + 1, i + 2, i + 3, i + 4 ])
+        t.end()
+    })
+}
+
+test('range should return an empty array when the end is less than the start', t => {
+    const result = range(5, 4)
+
+    t.deepEqual(result, [])
+    t.end()
+})
+
+test('range should return an array sorted from 0 to start-1 whenever the end parameter has a data type of undefined', t => {
+    // declare x as an undefined variable
+    let x;
+    const result = range(4, x)
+
+    t.deepEqual(result, [0, 1, 2, 3])
     t.end()
 })
