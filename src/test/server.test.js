bd19a933960ccdb88ab3e91a229913523518e180
Jake Walsh <jkwalsh127@gmail.com>
2022-08-28 11:05:53 -0700

    improve logic of the route test and expand coverage for range test by including negative and float parameters

--- src/test/server.test.js
+++ src/test/server.test.js
@@ -6,18 +6,16 @@ const getResponse = async () => {
     const res = await axios.get('http://localhost:3500/v0/updates');
  
     const data = res.data;
-    console.log(data)
     return data;
 }
 
 test('test server routes, should receive an object with a key of updates', async(t) => {
-    const scope = await nock(`http://localhost:3500`)
+    nock(`http://localhost:3500`)
         .get(`/v0/updates`)
         .reply(200, { updates: ["no new updates"]});
-    const response = JSON.parse(scope.keyedInterceptors['GET http://localhost:3500/v0/updates'][0].body);
 
-    const data = await getResponse();
+    const expectedData = await getResponse();
 
-    t.deepEqual(data, response, 'json response should match expected');
+    t.deepEqual(expectedData, { updates: ["no new updates"]}, 'json response should match expected');
     t.end();
 })
