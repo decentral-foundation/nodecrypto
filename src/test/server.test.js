26904c6f70e1e97339f5081e3edbcdeb5705da65
Jake Walsh <jkwalsh127@gmail.com>
2022-08-27 18:46:56 -0700

    add second method for testing route

--- src/test/server.test.js
+++ src/test/server.test.js
@@ -1,5 +1,14 @@
 const nock = require('nock');
 const test = require('tape');
+const axios = require('axios');
+
+const getResponse = async () => {
+    const res = await axios.get('http://localhost:3500/v0/updates');
+ 
+    const data = res.data;
+    console.log(data)
+    return data;
+}
 
 test('test server routes, should receive an object with a key of updates', async(t) => {
     // because we are testing a server internally, we must allow localhost connections to test our routes
@@ -10,6 +19,14 @@ test('test server routes, should receive an object with a key of updates', async
     // retrieve the body from the intercepted route, and parse the string to return the expected JSON
     const response = JSON.parse(scope.keyedInterceptors['GET http://localhost:3500/v0/updates'][0].body);
 
+    // t.deepEqual(response, { updates :["no new updates"]}, 'json response should match expected');
     t.deepEqual(response, { updates :["no new updates"]}, 'json response should match expected');
+    t.end();
+})
+
+test('test server routes, should receive an object with a key of updates', async(t) => {
+    const data = await getResponse();
+
+    t.deepEqual(data, { updates :["no new updates"]}, 'json response should match expected');
     t.end();
 })
